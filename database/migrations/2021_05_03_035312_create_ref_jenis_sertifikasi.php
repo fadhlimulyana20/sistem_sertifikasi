518e2d57e729fc625d8b7ed55ece893127c6a74d
Fheyrian <mikoferdian0@gmail.com>
2021-05-03 11:32:31 +0700

    migrate ref_jenis_sertifikasi & unit_kompetensi_sertifikasi

---
+++ database/migrations/2021_05_03_035312_create_ref_jenis_sertifikasi.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RefJenisSertifikasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ref_jenis_sertifikasi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->timestamps();
+
+            $table->string('nama');
+            $table->string('keterangan');
+            $table->boolean('status_jenis_sertifikasi')->nullable();
+            $table->boolean('is_aktif')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ref_jenis_sertifikasi');
+    }
+}
---
+++ database/migrations/2021_05_03_035423_create_unit_kompetensi_sertifikasi.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UnitKompetensiSertifikasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('unit_kompetensi_sertifikasi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+
+            $table->timestamps();
+
+            $table->unsignedBigInteger('id_ref_jenis_sertifikasi');
+            $table->unsignedBigInteger('id_ref_kompetensi');
+            $table->boolean('is_aktif')->nullable();
+
+            $table->foreign('id_ref_jenis_sertifikasi')->references('id')->on('ref_jenis_sertifikasi')->onDelete('cascade');
+            $table->foreign('id_ref_kompetensi')->references('id')->on('ref_unit_kompetensi')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('unit_kompetensi_sertifikasi');
+    }
+}
