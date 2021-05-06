82ccfc0cbb27470a60b2e86e845d78194ff9cf8c
JoyEldo <72270272+JoyEldo@users.noreply.github.com>
2021-05-06 08:36:45 +0700

    Add files via upload

---
+++ 2021_05_05_092435_create_ref_unit_kompetensi_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRefUnitKompetensiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ref_unit_kompetensi', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('id');
+            $table->string('nama');
+            $table->boolean('is_aktif');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ref_unit_kompetensi');
+    }
+}
---
+++ 2021_05_05_100013_create_syarat_sertifikasi_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSyaratSertifikasiTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('syarat_sertifikasi', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('id');
+            $table->unsignedBigInteger('id_ref_jenis_sertifikasi');
+            $table->string('syarat');
+            $table->boolean('is_aktif');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('syarat_sertifikasi');
+    }
+}
