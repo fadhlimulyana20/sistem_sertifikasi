1694eec07140757d9484a3f140f28516ef6788ef
fadhlimulyana20 <baba.fadhli@gmail.com>
2021-06-06 04:39:04 +0700

    Menambahkan stisla

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -6,6 +6,11 @@ window._ = require('lodash');
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
+window.Popper = require('popper.js').default;
+window.$ = window.jQuery = require('jquery');
+
+require('bootstrap');
+
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
