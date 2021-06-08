162332adf6ee1f6a4cc894d887145c3a8903b113
F-Pawamatra <72060401+F-Pawamatra@users.noreply.github.com>
2021-06-08 18:54:26 +0700

    mengubah halaman login dan register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,4 +1,72 @@
-<x-guest-layout>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+        <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+      .form-signin{
+        width: 100%;
+        max-width: 360px;
+        padding: 15px;
+        margin: auto;
+        border-radius: 20px;
+        box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+      }
+      input {
+          margin-bottom: 10px;
+          text-align: left;
+      }
+      body {
+          display: block;
+          margin: 8px
+      }
+      .div-center{
+        min-height: 100%;
+        display: flex;
+        align-items: center;
+      }
+
+    </style>
+</head>
+<body>
+    <div class="div-center">
+        <form class="form-signin" method="POST" action="{{ route('login') }}">
+            <h1 class="h3 mb-3 font-weight-bold text-center">LOGIN</h1>
+            @csrf
+            <input id="email" class="form-control" type="email" name="email" :value="old('email')" placeholder="E-mail" required autofocus>
+            <label for="inputPassword" class="sr-only">Password</label>
+            <input type="password" id="password" name="password" class="form-control" required autocomplete="current-password" placeholder="Password" required="">
+            <div class="checkbox m-2">
+                <label><input type="checkbox" value="remember-me"> Remember me</label>
+            </div>
+            <button class="btn btn-lg btn-primary btn-block" type="submit">Log in</button>
+            <div class="flex items-center text-center justify-end mt-4">
+                <a href="{{ route('register') }}">
+                    {{ __('Create an account') }}
+                </a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -53,4 +121,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,69 @@
-<x-guest-layout>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <!-- Bootstrap core CSS -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+
+        <style>
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+      .form-register{
+        width: 100%;
+        max-width: 360px;
+        padding: 15px;
+        margin: auto;
+        border-radius: 20px;
+        box-shadow: 0 30px 60px 0 rgba(0,0,0,0.3);
+      }
+      input {
+          margin-bottom: 6px;
+          text-align:left;
+      }
+      body {
+          display: block;
+          margin: 8px
+      }
+      .div-center{
+        min-height: 100%;
+        display: flex;
+        align-items: center;
+      }
+    </style>
+</head>
+<body class="text-center">
+    <div class="div-center">
+        <form class="form-register" method="POST" action="{{ route('register') }}">
+            <h1 class="h3 mb-3 font-weight-bold">REGISTER</h1>
+            @csrf
+            <input id="name" class="form-control" type="name" name="name" :value="old('name')" placeholder="Name" required autofocus>
+            <input id="email" class="form-control" type="email" name="email" :value="old('email')" placeholder="E-mail" required autofocus>
+            <input type="password" id="password" name="password" class="form-control" required autocomplete="new-password" placeholder="Password" required="">
+            <input type="password" id="password_confirmation" name="password_confirmation" class="form-control" required placeholder="Confirm Password" required="">
+            <button class="btn btn-lg btn-primary btn-block mt-4" type="submit">Register</button>
+            <div class="flex items-center justify-end mt-4">
+                <a href="{{ route('login') }}">
+                    {{ __('Already registered?') }}
+                </a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
+
+{{-- <x-guest-layout>
     <x-auth-card>
         <x-slot name="logo">
             <a href="/">
@@ -16,7 +81,7 @@
             <div>
                 <x-label for="name" :value="__('Name')" />
 
-                <x-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-input class="block mt-1 w-full" id="name" type="text" name="name" :value="old('name')" required autofocus />
             </div>
 
             <!-- Email Address -->
@@ -56,4 +121,4 @@
             </div>
         </form>
     </x-auth-card>
-</x-guest-layout>
+</x-guest-layout> --}}
