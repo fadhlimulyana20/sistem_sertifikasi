f52d081d9f8c24497cfa79650e879fe54583153a
Fheyrian <mikoferdian0@gmail.com>
2021-06-09 19:22:49 +0700

    memperbaiki view edit ref_kegiatan agar menampilkan data terkini

--- resources/views/pages/ref_kegiatan/edit.blade.php
+++ resources/views/pages/ref_kegiatan/edit.blade.php
@@ -11,17 +11,20 @@
   </div>
 
   <div class="section-body">
-      <div class="container py-5">
+      <div class="container">
+            @if ($errors->any())
+                {{ implode('', $errors->all('<div>:message</div>')) }}
+            @endif
           <div class="bg-white p-3 rounded">
               <form action="{{ route('ref_kegiatan.update', $ref_kegiatan->id) }}" method="POST">
                   @csrf
                   @method('PUT')
                   <div class="mb-3">
                       <label class="form-label" for="nama_kegiatan">Nama Kegiatan</label>
-                      <input class="form-control" type="text" name="nama_kegiatan" id="nama_kegiatan" placeholder="nama"
+                      <input class="form-control" type="text" name="nama_kegiatan" id="nama_kegiatan" placeholder="nama" value="{{ $ref_kegiatan->nama_kegiatan }}"
                           required>
                       <label class="form-lable" for="deskripsi">Deskripsi</label>
-                      <input class="form-control" type="text" name="deskripsi" id="deskripsi" placeholder="deskripsi"
+                      <input class="form-control" type="text" name="deskripsi" id="deskripsi" placeholder="deskripsi" value="{{ $ref_kegiatan->deskripsi }}"
                           required>
                   </div>
                   <button class="btn btn-success" type="submit">Simpan</button>
