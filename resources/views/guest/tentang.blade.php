761303a9ed15ce28d1eaa57b2352a7b67a93c887
hadni17 <ip_larasati@student.uns.ac.id>
2021-06-13 21:39:02 +0700

    menambah view about

--- resources/views/guest/tentang.blade.php
+++ resources/views/guest/tentang.blade.php
@@ -5,5 +5,66 @@
 @section('title', 'Tentang')
 
 @section('content')
+<section class="section">
+    <div class="container marketing">
+        <h1></h1>
+        <div class="row featurette">
+            <div class="col-md-3 offset-md-2">
+                <img class="featurette-image img-fluid mx-auto" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" width="220" height="220" alt="Generic placeholder image">
+            </div>
+          <div class="col-md-6 offset-md-0 ml-auto">
+            <h2 class="featurette-heading">Sistem Sertifikasi</h2>
+            <blockquote class="blockquote">
+            <p style="text-align: justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam blandit mauris vitae vehicula rutrum. Aliquam quis nunc massa. Suspendisse ut maximus justo. Etiam blandit enim odio, sit amet finibus quam pharetra vulputate. Cras euismod congue leo. Duis lacus ipsum, posuere at porttitor et, tristique ac nunc. Suspendisse eget rhoncus magna, convallis efficitur nulla.</p>
+            </blockquote>
+          </div>
+        </div>
+    <hr class="featurette-divider">
+</div>
+<div class="container marketing">
+     <h1 style="text-align:center">team member</h1>
+    <div class="row">
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Ferdian Wijatmiko</p>
+        </div>
 
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Fery Adwarsyah Pawamatra</p>
+        </div>
+
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Fina Enno Rizki Oktavia Putri</p>
+        </div>
+
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Indah Paksi Larasati</p>
+        </div>
+
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Irfan Rafi Rizaldi</p>
+        </div>
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Joy Kristian Eldo</p>
+        </div>
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Muhammad Dwi Arfian</p>
+        </div>
+        <div class="col-md-3 ml-auto">
+            <img class="rounded-circle" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Generic placeholder image" width="140" height="140" >
+            <p class="title" style="text-align: center;">Muhammad Fadhli Putra Mulyana</p>
+        </div>
+      </div>
+     </div>
+    </div>
+   </div>
+
+  </section>
 @endsection
+
